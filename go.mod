e1ce9ee02e776c6c33894d897ba77ee212a7bfaa
lzzzzl <lzzzzl005@gmail.com>
2023-09-20 23:17:19 +0800

    add postgresql test

--- go.mod
+++ go.mod
@@ -7,14 +7,19 @@ require (
 	github.com/alecthomas/kingpin/v2 v2.3.2
 	github.com/gin-contrib/requestid v0.0.6
 	github.com/gin-gonic/gin v1.9.1
+	github.com/golang-migrate/migrate/v4 v4.16.2
 	github.com/jmoiron/sqlx v1.3.5
+	github.com/lib/pq v1.10.2
+	github.com/pkg/errors v0.9.1
 	github.com/rs/zerolog v1.30.0
+	github.com/stretchr/testify v1.8.3
 )
 
 require (
 	github.com/alecthomas/units v0.0.0-20211218093645-b94a6e3cc137 // indirect
 	github.com/bytedance/sonic v1.9.1 // indirect
 	github.com/chenzhuoyu/base64x v0.0.0-20221115062448-fe3a3abad311 // indirect
+	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/gabriel-vasile/mimetype v1.4.2 // indirect
 	github.com/gin-contrib/sse v0.1.0 // indirect
 	github.com/go-playground/locales v0.14.1 // indirect
@@ -22,6 +27,8 @@ require (
 	github.com/go-playground/validator/v10 v10.14.0 // indirect
 	github.com/goccy/go-json v0.10.2 // indirect
 	github.com/google/uuid v1.3.0 // indirect
+	github.com/hashicorp/errwrap v1.1.0 // indirect
+	github.com/hashicorp/go-multierror v1.1.1 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/klauspost/cpuid/v2 v2.2.4 // indirect
 	github.com/lann/builder v0.0.0-20180802200727-47ae307949d0 // indirect
@@ -32,9 +39,11 @@ require (
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
 	github.com/pelletier/go-toml/v2 v2.0.8 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/twitchyliquid64/golang-asm v0.15.1 // indirect
 	github.com/ugorji/go/codec v1.2.11 // indirect
 	github.com/xhit/go-str2duration/v2 v2.1.0 // indirect
+	go.uber.org/atomic v1.7.0 // indirect
 	golang.org/x/arch v0.3.0 // indirect
 	golang.org/x/crypto v0.9.0 // indirect
 	golang.org/x/net v0.10.0 // indirect
--- internal/app/repository/postgres_repository_test.go
+++ internal/app/repository/postgres_repository_test.go
@@ -3,13 +3,20 @@ package repository
 import (
 	"fmt"
 	"log"
+	"testing"
 
+	"github.com/golang-migrate/migrate/v4"
+	_ "github.com/golang-migrate/migrate/v4/database/postgres"
+	_ "github.com/golang-migrate/migrate/v4/source/file"
 	"github.com/jmoiron/sqlx"
+	_ "github.com/lib/pq"
+	"github.com/pkg/errors"
+	"github.com/stretchr/testify/assert"
 )
 
 var postgresDB *sqlx.DB
 
-func getPostgresDB() *sqlx.DB {
+func getPostgresDB() (*sqlx.DB, error) {
 	connStr := fmt.Sprintf("user=%s dbname=%s sslmode=disable password=%s",
 		postgresName, postgresName, postgresName)
 
@@ -22,9 +29,78 @@ func getPostgresDB() *sqlx.DB {
 	postgresDB.SetMaxOpenConns(100)
 	postgresDB.SetMaxIdleConns(10)
 
-	return postgresDB
+	return postgresDB, nil
 }
 
 // migrationSourcePath is a relative path to the collection storing all migration scripts
 const migrationSourcePath = "file://../../../scripts/migrations"
 const postgresName = "page_turner_pro"
+
+func upPostgresDB() (*sqlx.DB, error) {
+	db, err := getPostgresDB()
+	if err != nil {
+		return nil, errors.WithMessage(err, "failed to start postgres container")
+	}
+
+	// migrate postgres
+	localHostPostgresDSN := fmt.Sprintf(
+		"postgresql://%s:%s@localhost/%s?sslmode=disable",
+		postgresName, postgresName, postgresName,
+	)
+	m, err := migrate.New(migrationSourcePath, localHostPostgresDSN)
+	if err != nil {
+		return nil, errors.WithMessage(err, "failed to start migration process")
+	}
+
+	err = m.Up()
+	if err != nil {
+		return nil, errors.WithMessage(err, "failed to migrare process")
+	}
+
+	return db, nil
+}
+
+func downPostgresDB() (*sqlx.DB, error) {
+	db, err := getPostgresDB()
+	if err != nil {
+		return nil, errors.WithMessage(err, "failed to start postgres container")
+	}
+
+	// migrate postgres
+	localHostPostgresDSN := fmt.Sprintf(
+		"postgresql://%s:%s@localhost/%s?sslmode=disable",
+		postgresName, postgresName, postgresName,
+	)
+	m, err := migrate.New(migrationSourcePath, localHostPostgresDSN)
+	if err != nil {
+		return nil, errors.WithMessage(err, "failed to start migration process")
+	}
+
+	err = m.Down()
+	if err != nil {
+		return nil, errors.WithMessage(err, "failed to migrare process")
+	}
+
+	return db, nil
+}
+
+func TestGetPostgresDB(t *testing.T) {
+	db, err := getPostgresDB()
+
+	assert.NotNil(t, db)
+	assert.NoError(t, err)
+}
+
+func TestUpPostgresDB(t *testing.T) {
+	db, err := upPostgresDB()
+
+	assert.NotNil(t, db)
+	assert.NoError(t, err)
+}
+
+func TestDownPostgresDB(t *testing.T) {
+	db, err := downPostgresDB()
+
+	assert.NotNil(t, db)
+	assert.NoError(t, err)
+}
