1e6b60d1675373fff7b27ba1d48ef1d49f0841b8
lzzzzl <lzzzzl005@gmail.com>
2023-09-21 08:04:10 +0800

    add book model

--- internal/domain/model/book.go
+++ internal/domain/model/book.go
@@ -1,1 +1,22 @@
 package model
+
+import "time"
+
+type Book struct {
+	ID            int
+	Title         string
+	Author        string
+	PublishedYear time.Time
+	ISBN          string
+	CreatedAt     time.Time
+	UpdatedAt     time.Time
+}
+
+func NewBook(title, author, isbn string, publishedYear time.Time) Book {
+	return Book{
+		Title:         title,
+		Author:        author,
+		ISBN:          isbn,
+		PublishedYear: publishedYear,
+	}
+}
