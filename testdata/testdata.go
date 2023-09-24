589b61742966bc4bb68b9c66b408ef2fd8d8b217
lzzzzl <lzzzzl005@gmail.com>
2023-09-24 15:12:55 +0800

    add testdata for user

---
+++ testdata/testdata.go
@@ -0,0 +1,21 @@
+package testdata
+
+import (
+	"path/filepath"
+	"runtime"
+)
+
+var basepath string
+
+const (
+	TestDataUser = "user.yaml"
+)
+
+func init() {
+	_, currentFile, _, _ := runtime.Caller(0)
+	basepath = filepath.Dir(currentFile)
+}
+
+func Path(rel string) string {
+	return filepath.Join(basepath, rel)
+}
